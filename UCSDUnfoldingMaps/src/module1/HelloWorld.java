120eceff64f070761cef3993c7dbf7333e3c2670
kedarw <kedarwaghmode@gmail.com>
2016-03-03 22:10:08 -0800

    Module1 complete project as per following requirements:
    Add code to the file so that your applet does the following (see the hints that follow for help if you need it):
    
    Display a second map next to the default one. This map should be zoomed into your hometown by default -- see hints
    below for how to find your hometown latitude and longitude. Use a different Map Provider for each of the two maps.
    If you are working offline, you should not change the map provider, but you should use the “dark” tiles which are
    in the file “blankLight-1-3.mbtiles.” For some Map Provider choices for working online, see the hints below.
    Change the background of the display to be a different color. BONUS: can you include a visual effect in the background (shading / gradient)?

--- UCSDUnfoldingMaps/src/module1/HelloWorld.java
+++ UCSDUnfoldingMaps/src/module1/HelloWorld.java
@@ -11,8 +11,8 @@ import de.fhpotsdam.unfolding.utils.MapUtils;
 /** HelloWorld
   * An application with two maps side-by-side zoomed in on different locations.
   * Author: UC San Diego Coursera Intermediate Programming team
-  * @author Your name here
-  * Date: July 17, 2015
+  * @author Kedar Waghmode
+  * Date: December 12, 2015
   * */
 public class HelloWorld extends PApplet
 {
@@ -43,7 +43,7 @@ public class HelloWorld extends PApplet
 
 		// This sets the background color for the Applet.  
 		// Play around with these numbers and see what happens!
-		this.background(200, 200, 200);
+		this.background(0, 255, 0);
 		
 		// Select a map provider
 		AbstractMapProvider provider = new Google.GoogleTerrainProvider();
@@ -70,20 +70,26 @@ public class HelloWorld extends PApplet
 		// The next line zooms in and centers the map at 
 	    // 32.9 (latitude) and -117.2 (longitude)
 	    map1.zoomAndPanTo(zoomLevel, new Location(32.9f, -117.2f));
-		
+
 		// This line makes the map interactive
 		MapUtils.createDefaultEventDispatcher(this, map1);
 		
-		// TODO: Add code here that creates map2 
-		// Then you'll modify draw() below
+		// Create new map object drawn to be
+		map2 = new UnfoldingMap(this, 420, 50, 350, 500, provider);
 
+		// 18.07 (latituge) and 74.74 (longitude)
+		// My home town Lasurne in state of Maharashtra, India
+		map2.zoomAndPanTo(zoomLevel, new Location(18.07f, 74.74f));
+
+		MapUtils.createDefaultEventDispatcher(this, map1);
 	}
 
 	/** Draw the Applet window.  */
 	public void draw() {
-		// So far we only draw map1...
-		// TODO: Add code so that both maps are displayed
 		map1.draw();
+
+		// Draw new map on applet
+		map2.draw();
 	}
 
 	

a420ca5230949672ef5a4d6f285fde65ec04f179
kedarw <kedarwaghmode@gmail.com>
2016-03-05 09:37:46 -0800

    As part of module4 assignment added code as per following requirements:
    1) Implement the isLand(Feature earthquake) method in EarthquakeCityMap. This method should return true if
    the location of the input earthquake is on land. It should also set the "country" property on the LandMarker
    to the country where the earthquake occurred. Otherwise, the location is in the ocean and the method should return false.
    
    2) Implement the printQuakes() method in EarthquakeCityMap. This method should use System.out.println() to list
    each country for which there was 1 or more earthquakes and the number of earthquakes detected in that country.
    Then it should print out the number of quakes that were detected in the ocean. Note that this method is not trivial.
    You will have to calculate the number of earthquakes per country from the information you have available. As an aside:
    If you are running the applet with a large earthquake file/feed (e.g. 1.0+ Past week or 30 days), you might find that
    printQuakes takes a long time to run.
    
    3) Complete the method definition for the draw() method in the CityMarker class. We suggest you use triangles to
    represent cities, but of course the exact shape and color is up to you. Then add the marker to your key (which
    you will need to modify as you change your earthquake markers). Your map should now look something like the image below.
    
    4)  Complete the method definitions for the drawEarthquake() method in LandQuakeMarker and OceanQuakeMarker so that
    the size of the marker varies by its magnitude (larger magnitude=larger marker). In addition, make the LandQuakeMarkers
    one shape (we suggest circles) and the OceanQuakeMarkers a different shape (we suggest squares). Be sure to center
    them at the x, y position passed in to drawEarthquake. Also, they will appear white for now. That’s OK. Do not set their
    color in drawEarthquake. That will be done in the next step. Then add your markers to the key. Your map should now
    look something like the image below. Make sure there is a visual difference between the size of the markers for
    different magnitudes.
    
    5) Complete the method definition for colorDetermine() method in EarthquakeMarker and notice where it and the methods
    from the derived classes are called in the EarthquakeMarker’s draw method.
    
    6)  Modify the draw method so that it draws an X over all earthquake markers whose earthquakes have occurred in the last day.

--- UCSDUnfoldingMaps/src/module4/LandQuakeMarker.java
+++ UCSDUnfoldingMaps/src/module4/LandQuakeMarker.java
@@ -4,19 +4,17 @@ import de.fhpotsdam.unfolding.data.PointFeature;
 import processing.core.PGraphics;
 
 /** Implements a visual marker for land earthquakes on an earthquake map
- * 
+ *
  * @author UC San Diego Intermediate Software Development MOOC team
- * @author Your name here
+ * @author Kedar Waghmode
  *
  */
 public class LandQuakeMarker extends EarthquakeMarker {
-	
-	
+
 	public LandQuakeMarker(PointFeature quake) {
-		
+
 		// calling EarthquakeMarker constructor
 		super(quake);
-		
 		// setting field in earthquake marker
 		isOnLand = true;
 	}
@@ -28,21 +26,27 @@ public class LandQuakeMarker extends EarthquakeMarker {
 		// DO NOT set the fill color here.  That will be set in the EarthquakeMarker
 		// class to indicate the depth of the earthquake.
 		// Simply draw a centered circle.
-		
+
 		// HINT: Notice the radius variable in the EarthquakeMarker class
 		// and how it is set in the EarthquakeMarker constructor
-		
-		// TODO: Implement this method
-		
+
+		pg.pushStyle();
+
+		java.util.HashMap<String, Object> properties = this.getProperties();
+		float magnitude = Float.parseFloat(properties.get("magnitude").toString());
+		properties.put("radius", 2*magnitude );
+		setProperties(properties);
+		this.radius = 1.75f*getMagnitude();
+
+		pg.ellipse(x, y, radius, radius);
+		// Restore previous drawing style
+		pg.popStyle();
 	}
-	
+
 
 	// Get the country the earthquake is in
 	public String getCountry() {
 		return (String) getProperty("country");
 	}
 
-
-
-		
 }
--- UCSDUnfoldingMaps/src/module4/OceanQuakeMarker.java
+++ UCSDUnfoldingMaps/src/module4/OceanQuakeMarker.java
@@ -1,23 +1,21 @@
 package module4;
 
 import de.fhpotsdam.unfolding.data.PointFeature;
+import de.fhpotsdam.unfolding.marker.SimplePointMarker;
 import processing.core.PGraphics;
 
 /** Implements a visual marker for ocean earthquakes on an earthquake map
- * 
+ *
  * @author UC San Diego Intermediate Software Development MOOC team
- * @author Your name here
+ * @author Kedar Waghmode
  *
  */
 public class OceanQuakeMarker extends EarthquakeMarker {
-	
 	public OceanQuakeMarker(PointFeature quake) {
 		super(quake);
-		
 		// setting field in earthquake marker
 		isOnLand = false;
 	}
-	
 
 	@Override
 	public void drawEarthquake(PGraphics pg, float x, float y) {
@@ -25,16 +23,18 @@ public class OceanQuakeMarker extends EarthquakeMarker {
 		// DO NOT set the fill color.  That will be set in the EarthquakeMarker
 		// class to indicate the depth of the earthquake.
 		// Simply draw a centered square.
-		
 		// HINT: Notice the radius variable in the EarthquakeMarker class
 		// and how it is set in the EarthquakeMarker constructor
-		
-		// TODO: Implement this method
-		
+		pg.pushStyle();
+
+		java.util.HashMap<String, Object> properties = this.getProperties();
+		float magnitude = Float.parseFloat(properties.get("magnitude").toString());
+		properties.put("radius", 2*magnitude );
+		setProperties(properties);
+		this.radius = 1.75f*getMagnitude();
+
+		pg.rect(x, y, radius, radius);
+		// Restore previous drawing style
+		pg.popStyle();
 	}
-	
-
-
-	
-
 }

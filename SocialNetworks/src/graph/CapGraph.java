2c4055cf55eab9fe1869949010a4ab3038da672d
kedarw <kedarwaghmode@gmail.com>
2016-09-21 18:59:37 -0700

    Updated code for author details

--- SocialNetworks/src/graph/CapGraph.java
+++ SocialNetworks/src/graph/CapGraph.java
@@ -15,8 +15,8 @@ import java.util.Map.Entry;
 import util.GraphLoader;
 
 /**
- * @author Your name here.
- * 
+ * @author Kedar Waghmode
+ *
  * For the warm up assignment, you must implement your Graph in a class
  * named CapGraph.  Here is the stub file.
  *
@@ -24,17 +24,17 @@ import util.GraphLoader;
 public class CapGraph implements Graph {
 	private int numVertices;
 	private int numEdges;
-	
-	//Map to represent vertex and edges pointing to other vertices 
+
+	//Map to represent vertex and edges pointing to other vertices
 	private Map<Integer,ArrayList<Integer>> adjListsMap;
-	
+
 	//Create an empty graph
 	public CapGraph () {
 		adjListsMap = new HashMap<Integer,ArrayList<Integer>>();
 		numVertices = 0;
 		numEdges = 0;
 	}
-	
+
 	/* (non-Javadoc)
 	 * @see graph.Graph#addVertex(int)
 	 */
@@ -62,7 +62,7 @@ public class CapGraph implements Graph {
 		CapGraph graph = new CapGraph();
 		Iterator<Entry<Integer, ArrayList<Integer>>> it = adjListsMap.entrySet().iterator();
 		Boolean found = false;
-		
+
 		while(it.hasNext()) {
 			Map.Entry pair = (Map.Entry)it.next();
 			if(center == (int) pair.getKey()) {
@@ -75,10 +75,8 @@ public class CapGraph implements Graph {
 			}
 		}
 		if(found) {
-			
 		}
-		
-		
+
 		return graph;
 	}
 
@@ -97,23 +95,21 @@ public class CapGraph implements Graph {
 	@Override
 	public HashMap<Integer, HashSet<Integer>> exportGraph() {
 		HashMap<Integer, HashSet<Integer>> complete_graph = new HashMap<Integer, HashSet<Integer>>();
-		
+
 		Iterator<Entry<Integer, ArrayList<Integer>>> it = adjListsMap.entrySet().iterator();
-		
+
 		while (it.hasNext()) {
 			Map.Entry pair = (Map.Entry)it.next();
 			int vertex = (int) pair.getKey();
 			ArrayList<Integer> edges = (ArrayList<Integer>) pair.getValue();
 			HashSet<Integer> neighbors = new HashSet<Integer>(edges);
-			
+
 			complete_graph.put(vertex, neighbors);
 		}
-		
 		return complete_graph;
 	}
-	
+
 	public void displayGraph() {
-		
 		for(int v : adjListsMap.keySet())
 		{
 			System.out.print(v + " --> ");
@@ -124,26 +120,25 @@ public class CapGraph implements Graph {
 			System.out.println();
 		}
 	}
-	
+
 	public static void main(String [] args)
 	{
 		CapGraph small_graph = new CapGraph();
-		
+
 		small_graph.addVertex(1);
 		small_graph.addVertex(2);
 		small_graph.addVertex(3);
-		
+
 		small_graph.addEdge(1, 2);
 		small_graph.addEdge(2, 3);
 		small_graph.addEdge(3, 1);
-		
+
 		System.out.println("Displaying very small raw graph");
 		small_graph.displayGraph();
-		
+
 		CapGraph small_test_graph = new CapGraph();
 		GraphLoader.loadGraph(small_test_graph, "data/small_test_graph.txt");
 		System.out.println("Displaying small test graph");
 		small_test_graph.displayGraph();
 	}
-
 }
